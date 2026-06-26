26cae6bedcd754455ef2ce55efe8eb090fb86b63
Juana Hernández cortez <csarielscontacto@gmail.com>
2026-06-26 00:25:36 -0600

    Add weather adjustment function for bakery

---
+++ ia-predictiva/analizador_entorno.js
@@ -0,0 +1,6 @@
+// Analizador de Entorno - Sariel's Bakery
+function obtenerAjustePorClima() {
+    const clima = "soleado"; // En el futuro, esto se conectará a internet
+    return (clima === "lluvioso") ? 0.8 : 1.2;
+}
+
