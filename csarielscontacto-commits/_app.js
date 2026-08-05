022d733915f06315a5d4bacb56b60c03e3a50ca8
Juana Hernández cortez <csarielscontacto@gmail.com>
2026-08-05 15:22:20 -0600

    Eliminar _app.js

--- csarielscontacto-commits/_app.js
+++
@@ -1,6 +0,0 @@
-import '../config/i18n';
-import '../styles/globals.css'; // O el nombre de tu css global si lo tienes
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
