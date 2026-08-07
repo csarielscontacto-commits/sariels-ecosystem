8477737183e9e3363bd19cd37fd1a7d56d40b34e
Juana Hernández cortez <csarielscontacto@gmail.com>
2026-08-07 03:46:53 -0600

    Eliminar _app.js

--- csarielscontacto-commits/js/_app.js
+++
@@ -1,6 +0,0 @@
-// js/_app.js
-import './i18n.js'; // Importa el motor de idiomas desde la misma carpeta js
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
